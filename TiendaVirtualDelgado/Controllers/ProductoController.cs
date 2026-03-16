da0ecf65edc330ac2076be7216c26e4bc3061089
sebastian <sedadelo123@gmail.com>
2026-03-16 17:53:48 -0500

    conexion base de datos

--- TiendaVirtualDelgado/Controllers/ProductoController.cs
+++ TiendaVirtualDelgado/Controllers/ProductoController.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography.X509Certificates;
+using TiendaVirtualDelgado.Data;
+using Microsoft.EntityFrameworkCore;
 using TiendaVirtualDelgado.Models;
 
 namespace TiendaVirtualDelgado.Controllers
 {
     public class ProductoController : Controller
     {
+        private readonly TiendaContext _context;
+
+        public ProductoController(TiendaContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            var productos = new List<Producto>
-            {
-                new Producto { Id = 1, Nombre = "laptop", Precio = 3000, Stock = 5},
-                new Producto { Id = 2, Nombre = "Mouse", Precio = 80, Stock = 0 },
-            };
+            var productos = _context.productos
+                .Include(p => p.Categoria)
+                .ToList();
+
             return View(productos);
-        }
+        }       
     }
 }
